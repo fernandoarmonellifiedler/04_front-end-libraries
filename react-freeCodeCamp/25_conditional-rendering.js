393b76394eb46b246278e4fbe85a50d7ebea1450
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-01-21 02:01:45 -0300

    new react lessons

---
+++ react-freeCodeCamp/25_conditional-rendering.js
@@ -0,0 +1,145 @@
+/* Render with an If-Else Condition
+
+Another application of using JavaScript to control your rendered view is to tie the elements that are rendered to a condition. When the condition is true, one view renders. When it's false, it's a different view. You can do this with a standard if/else statement in the render() method of a React component.
+
+-------------------------------
+
+Exercise: MyComponent contains a boolean in its state which tracks whether you want to display some element in the UI or not. The button toggles the state of this value. Currently, it renders the same UI every time. Rewrite the render() method with an if/else statement so that if display is true, you return the current markup. Otherwise, return the markup without the h1 element.
+
+Note: You must write an if/else to pass the tests. Use of the ternary operator will not pass here. */
+
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: true
+        }
+        this.toggleDisplay = this.toggleDisplay.bind(this);
+    }
+    toggleDisplay() {
+        this.setState((state) => ({
+            display: !state.display
+        }));
+    }
+    render() {
+        // Change code below this line
+        if (this.state.display) {
+            return (
+                <div>
+                    <button onClick={this.toggleDisplay}>Toggle Display</button>
+                    <h1>Displayed!</h1>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <button onClick={this.toggleDisplay}>Toggle Display</button>
+                </div>
+            )
+        }
+    }
+};
+
+// ----------------------------
+/* Use && for a More Concise Conditional
+
+The if/else statements worked in the last challenge, but there's a more concise way to achieve the same result. Imagine that you are tracking several conditions in a component and you want different elements to render depending on each of these conditions. If you write a lot of else if statements to return slightly different UIs, you may repeat code which leaves room for error. Instead, you can use the && logical operator to perform conditional logic in a more concise way. This is possible because you want to check if a condition is true, and if it is, return some markup. Here's an example:
+
+{condition && <p>markup</p>}
+
+If the condition is true, the markup will be returned. If the condition is false, the operation will immediately return false after evaluating the condition and return nothing. You can include these statements directly in your JSX and string multiple conditions together by writing && after each one. This allows you to handle more complex conditional logic in your render() method without repeating a lot of code.
+
+-------------------------------
+
+Exercise: Solve the previous example again, so the h1 only renders if display is true, but use the && logical operator instead of an if/else statement.*/
+
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: true
+        }
+        this.toggleDisplay = this.toggleDisplay.bind(this);
+    }
+    toggleDisplay() {
+        this.setState(state => ({
+            display: !state.display
+        }));
+    }
+    render() {
+        // Change code below this line
+
+        return (
+
+            <div>
+                <button onClick={this.toggleDisplay}>Toggle Display</button>
+                {this.state.display && <h1>Displayed!</h1>}
+            </div>
+        );
+    }
+};
+
+
+// ----------------------------
+/* Use a Ternary Expression for Conditional Rendering
+
+Before moving on to dynamic rendering techniques, there's one last way to use built-in JavaScript conditionals to render what you want: the ternary operator. The ternary operator is often utilized as a shortcut for if/else statements in JavaScript. They're not quite as robust as traditional if/else statements, but they are very popular among React developers. One reason for this is because of how JSX is compiled, if/else statements can't be inserted directly into JSX code. You might have noticed this a couple challenges ago — when an if/else statement was required, it was always outside the return statement. Ternary expressions can be an excellent alternative if you want to implement conditional logic within your JSX. Recall that a ternary operator has three parts, but you can combine several ternary expressions together. Here's the basic syntax:
+
+condition ? expressionIfTrue : expressionIfFalse;
+
+-------------------------------
+
+Exercise: The code editor has three constants defined within the CheckUserAge component's render() method. They are called buttonOne, buttonTwo, and buttonThree. Each of these is assigned a simple JSX expression representing a button element. First, initialize the state of CheckUserAge with input and userAge both set to values of an empty string.
+
+Once the component is rendering information to the page, users should have a way to interact with it. Within the component's return statement, set up a ternary expression that implements the following logic: when the page first loads, render the submit button, buttonOne, to the page. Then, when a user enters their age and clicks the button, render a different button based on the age. If a user enters a number less than 18, render buttonThree. If a user enters a number greater than or equal to 18, render buttonTwo.*/
+
+const inputStyle = {
+    width: 235,
+    margin: 5
+};
+
+class CheckUserAge extends React.Component {
+    constructor(props) {
+        super(props);
+        // Change code below this line
+        this.state = {
+            input: '',
+            userAge: ''
+        }
+        // Change code above this line
+        this.submit = this.submit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e) {
+        this.setState({
+            input: e.target.value,
+            userAge: ''
+        });
+    }
+    submit() {
+        this.setState(state => ({
+            userAge: state.input
+        }));
+    }
+    render() {
+        const buttonOne = <button onClick={this.submit}>Submit</button>;
+        const buttonTwo = <button>You May Enter</button>;
+        const buttonThree = <button>You Shall Not Pass</button>;
+        return (
+            <div>
+                <h3>Enter Your Age to Continue</h3>
+                <input
+                    style={inputStyle}
+                    type='number'
+                    value={this.state.input}
+                    onChange={this.handleChange}
+                />
+                <br />
+                {/* Change code below this line */}
+
+                {this.state.userAge === '' ? buttonOne : this.state.userAge < 18 ? buttonThree : buttonTwo}
+                {/* Change code above this line */}
+            </div>
+        );
+    }
+}
