a15ba991e3d148b9c8b8401bed6c2e1f8d12adb4
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-03-08 18:12:34 -0300

    acordion app

--- josh-smilga_tutorials/code-15-react-projects/react-projects-master/4-accordion/my-app/src/Question.js
+++ josh-smilga_tutorials/code-15-react-projects/react-projects-master/4-accordion/my-app/src/Question.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  const [questionState, setQuestionState] = useState(false);
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button
+          className='btn'
+          onClick={() => setQuestionState(!questionState)}
+        >
+          {questionState ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {questionState && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
