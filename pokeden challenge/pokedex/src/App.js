12dc30fbf750727a15c6b65fd5be6dbd6967c23e
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-03-30 01:43:18 -0300

    pokedex challenge

---
+++ pokeden challenge/pokedex/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Pokedex from './components/Pokedex'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <div className="section">
+        <Pokedex />
+      </div>
+      <div className="results">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
