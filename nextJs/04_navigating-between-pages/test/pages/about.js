32e357ddfaa321146b370c19ea15447b5eadb8c5
Fernando Personal <fernando.armonellifiedler@avature.net>
2021-05-28 23:02:57 -0300

    add new lesson nextjs

---
+++ nextJs/04_navigating-between-pages/test/pages/about.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header.js'
+
+const About = () => {
+  return (
+    <>
+    <Header />
+      <h1>This is the About Page</h1>
+    </>
+  )
+}
+
+export default About
---
+++ nextJs/04_navigating-between-pages/test/pages/contact.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header.js'
+
+const Contact = () => {
+  return (
+    <>
+    <Header />
+      <h1>This is the Contact Page</h1>
+    </>
+  )
+}
+
+export default Contact
